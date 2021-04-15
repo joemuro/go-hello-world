a379bacd3f93f7aebdb9b9e6cce768ef2e09c68b
Joe Muro <jmuro@cloudbees.com>
2021-04-14 22:55:31 -0400

    Print use(ful/less) things

--- hello-world.go
+++ hello-world.go
@@ -1,6 +1,16 @@
 package main
 import "fmt"
+import "os"
+import "runtime"
 
 func main() {
     fmt.Println("hello world")
+    fmt.Printf("PID: %d\n", os.Getpid())
+    fmt.Printf("UID: %d\n", os.Getuid())
+    cwd,_ := os.Getwd()
+    fmt.Printf("cwd: %s\n", cwd)
+    hostname,_ := os.Hostname()
+    fmt.Printf("hostname: %s\n",hostname) 
+    fmt.Printf("NumCPUs: %d\n",runtime.NumCPU())
+    fmt.Printf("golang-version: %s\n", runtime.Version())
 }
